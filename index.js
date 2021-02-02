50bb87f207e6d2bcb036d95ad6cb2934ef689cdc
scott95100 <scott95100@gmail.com>
2021-02-02 14:57:53 -0700

    finishes post route to add new dino

--- index.js
+++ index.js
@@ -1,52 +1,73 @@
-const express = require(`express`)
+const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const fs = require('fs')
 
-//middleWare
-//this will help us use our layout file(layout.ejs)
-app.use(expressLayouts)
 
+// MiddleWare
+// this will help us use our layout file
+app.use(expressLayouts)
+app.use(express.urlencoded({extended: false}));
+
+// for views use .ejs files
 app.set('view engine', 'ejs')
 
-//ROUTES
+// ROUTES
 app.get('/', (req, res)=> {
-    res.send('heyo')
+    res.send('Hi there')
 })
-//Create new view 
-app.get('dinosaurs/new', (rec, res)=> {
-    res.render('dinosaurs/new')
-})
-//POST route, doesnt have a view
-app.post('/dinosaurs', (req, res)=> {
-    // this is coming from our form submit
-    //we are going to look at the req.body
-    console.log(req.body)
-})
-//index view 
-app.get('/dinosaurs', (rec, res)=> {
-    // take our data and put it in a more readable format
+
+// Index View
+// this url: localhost:8000/dinosaurs
+app.get('/dinosaurs', (req, res)=> {
     let dinos = fs.readFileSync('./dinosaurs.json')
+    // take our data and put it in a more readable format
     dinos = JSON.parse(dinos)
-    //in our views folder render this page
-    res.render('dinosaurs/index', { dinos: dinos })
-} )
+    console.log(dinos)
+    // in our views folder render this page
+    res.render('dinosaurs/index', {dinos: dinos} )
+})
 
+// NEW View
+// Most specific to least specific url path
+app.get('/dinosaurs/new', (req, res)=> {
+    res.render('dinosaurs/new')
+  })
 
-//SHOW view
+// SHOW View
 app.get('/dinosaurs/:index', (req, res)=> {
     let dinos = fs.readFileSync('./dinosaurs.json')
+    // take our data and put it in a more readable format
     dinos = JSON.parse(dinos)
+    // get the dino that's asked for
+    // req.params.index
     const dino = dinos[req.params.index]
     res.render('dinosaurs/show', { dino })
 })
 
 
 
+// POST route, doesn't have a view
 
+app.post('/dinosaurs', (req, res)=> {
+    let dinos = fs.readFileSync('./dinosaurs.json')
+    // take our data and put it in a more readable format
+    dinos = JSON.parse(dinos)
+    //construct a new dino with our req.body values
+    const newDino = {
+        name: req.body.name,
+        type: req.body.type
+    }
+    //updates dinos with new dino
+    dinos.push(newDino)
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos))
+    //get request to /dinosaurs
+    res.redirect('/dinosaurs')
+    // this is coming from our form submit
+    // we are going to look at the req.body
+    
 
-
-
+})
 
 
 const PORT = process.env.PORT || 8000
